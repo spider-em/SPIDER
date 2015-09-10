a6af873bac6c8b0af62986580b1bdd44e57f2b4b
ArDean Leith <leith@wadsworth.org>
2015-09-10 09:45:00 -0400

    GOTO within LB doloop bug

--- src/findlbq.f
+++ src/findlbq.f
@@ -1,23 +1,25 @@
 
 C++*********************************************************************
 C
-C  FINDLBQ.F -- ADAPTED FOR CHAR.                  AUG  89 ArDean Leith
-C               MERGED WITH SEARCHQ STUFF          SEPT 97 ArDean Leith
-C               ADDED IFLEVEL                      DEC. 97 ArDean Leith
-C               INCORE PROCS                       JAN  01 ArDean Leith
-C               LUNDONOW                           FEB  01 ArDean Leith
-C               LNBLNKN                            MAY  04 ArDean Leith
-C               DOC INSIDE LOOP BUG                JUL  07 ArDean Leith
-C               NINSAVEOF                          NOV  09 ArDean Leith
-C               PASS LOOP END WHILE HUNTING LB     DEC  09 Ardean Leith
-C               PASS LB6 WHILE HUNTING LB7 BUG     JAN  10 Ardean Leith
-C               IF CYCLE WITHIN DO BUG             JAN  10 Ardean Leith
-C               ! COMMENT DELIMITER                DEC  11 Ardean Leith
+C  FINDLBQ.F    ADAPTED FOR CHAR.                 AUG  89 ArDean Leith
+C               MERGED WITH SEARCHQ STUFF         SEPT 97 ArDean Leith
+C               ADDED IFLEVEL                     DEC. 97 ArDean Leith
+C               INCORE PROCS                      JAN  01 ArDean Leith
+C               LUNDONOW                          FEB  01 ArDean Leith
+C               LNBLNKN                           MAY  04 ArDean Leith
+C               DOC INSIDE LOOP BUG               JUL  07 ArDean Leith
+C               NINSAVEOF                         NOV  09 ArDean Leith
+C               PASS LOOP END WHILE HUNTING LB    DEC  09 Ardean Leith
+C               PASS LB6 WHILE HUNTING LB7 BUG    JAN  10 Ardean Leith
+C               IF CYCLE WITHIN DO BUG            JAN  10 Ardean Leith
+C               ! COMMENT DELIMITER               DEC  11 Ardean Leith
+C               GOTO LB WITHIN LOOP BUG           SEP  15 Ardean Leith
+C
 C **********************************************************************
 C=*                                                                    *
 C=* This file is part of:   SPIDER - Modular Image Processing System.  *
 C=* SPIDER System Authors:  Joachim Frank & ArDean Leith               *
-C=* Copyright 1985-2011  Health Research Inc.,                         *
+C=* Copyright 1985-2015  Health Research Inc.,                         *
 C=* Riverview Center, 150 Broadway, Suite 560, Menands, NY 12204.      *
 C=* Email: spider@wadsworth.org                                        *
 C=*                                                                    *
@@ -67,6 +69,8 @@ C--*******************************************************************
         INTEGER                :: DOLABEL
         INTEGER                :: NEWLOOP 
 
+        INTEGER                :: ICOMM,MYPID,MPIERR
+
         CALL SET_MPI(ICOMM,MYPID,MPIERR) ! SETS ICOMM AND MYPID
 
 	NEWLOOP    = 0
@@ -78,7 +82,7 @@ C       KEEP READING AND DISCARDING LINES FROM INPUT AS NEEDED
         DO WHILE (KEEPGO) 
            ANSW(4:4) = ' '
 
-           IF (COPT .EQ. 'B' .AND. NINSAVEOF .EQ. 0) THEN
+           IF (COPT == 'B' .AND. NINSAVEOF == 0) THEN
 C             INCREMENT PROCEDURE LINE COUNTER
               IBCNT = IBCNT + 1
 
@@ -88,7 +92,7 @@ C             READ FROM CURRENT STORED PROCEDURE LINE= IBCNT
 C             READ FROM FILE OPENED ON NIN (FOR INTERACTIVE LOOP)
               READ(NIN,FMT='(A)',IOSTAT=IOS) ANSW
               NCHAR = LNBLNKN(ANSW)
-              IF (LUNDONOW .GT. 0) THEN
+              IF (LUNDONOW > 0) THEN
 C               MUST COPY INPUT LINE TO CURRENT INTERACTIVE DO-LOOP FILE
                 WRITE(LUNDONOW,*) ANSW(1:NCHAR)
               ENDIF
@@ -107,11 +111,11 @@ C          REMOVE BLANKS FROM INPUT STRING
 
 C          IGNORE ANY COMMENTS AT END OF INPUT STRING
            ISEMICOL = SCAN(ANSW(1:NCHAR),';!')
-           IF (ISEMICOL .GT. 0) THEN
+           IF (ISEMICOL > 0) THEN
               ANSW(ISEMICOL:NCHAR) = CHAR(0)
               NCHAR = ISEMICOL - 1
            ENDIF
-           IF (NCHAR .LE. 0) CYCLE
+           IF (NCHAR <= 0) CYCLE
 
 C          CONVERT INPUT STRING TO ALL UPPER CASE
            CALL SSUPCAS(ANSW(1:NCHAR))
@@ -125,45 +129,45 @@ C          FIND LAST NON-BLANK IN WANTLABEL
 C          FIND LAST NON-BLANK IN ANSW            
            LENANSW = LNBLNKN(ANSW)
            LENT    = INDEX(ANSW,CHAR(0)) - 1
-           IF (LENT .GT. 0) LENANSW = MIN(LENANSW,LENT)
+           IF (LENT > 0) LENANSW = MIN(LENANSW,LENT)
 
         !write(6,*)' want: ',wantlabel(:lenlb),' answ: ',answ(1:lenansw)
 
 C          -------------------------------------- SEARCHING FOR "ENDDO"
 C          DO NOT CARE ABOUT IF'S, OR LABELS IN HERE
 
-          IF (WANTLABEL(:LENLB) .EQ. 'ENDDO' .AND.
-     &         ANSW(1:2)         .EQ. 'DO'   .AND.
-     &         ANSW(3:3)         .NE. 'C'    .AND.
-     &         ANSW(3:4)         .NE. 'LB') THEN
+          IF (WANTLABEL(:LENLB) ==  'ENDDO' .AND.
+     &         ANSW(1:2)        ==  'DO'    .AND.
+     &         ANSW(3:3)       .NE. 'C'     .AND.
+     &         ANSW(3:4)       .NE. 'LB') THEN
 
 C              HUNTING FOR 'ENDDO' AND PASSING NESTED 'DO...ENDDO'
                NEEDENDO = NEEDENDO + 1
                !write(6,*) ' needendop1:',needendo
 
-           ELSEIF (WANTLABEL(:LENLB) .EQ. 'ENDDO' .AND.
-     &             ANSW(1:2)      .EQ. 'IF' .AND.
-     &             IIFTHEN .GT. 5 ) THEN
+           ELSEIF (WANTLABEL(:LENLB) == 'ENDDO' .AND.
+     &             ANSW(1:2)      == 'IF' .AND.
+     &             IIFTHEN > 5 ) THEN
 C              HUNTING FOR 'ENDDO' AND FOUND A NEW 'IF...THEN' CLAUSE
                NEEDENDIF = NEEDENDIF + 1
 
-           ELSEIF (WANTLABEL(:LENLB) .EQ. 'ENDDO' .AND.
-     &             ANSW(1:5)      .EQ. 'ENDIF') THEN
+           ELSEIF (WANTLABEL(:LENLB) == 'ENDDO' .AND.
+     &             ANSW(1:5)      == 'ENDIF') THEN
 C              HUNTING FOR 'ENDDO' AND FOUND AN 'ENDIF' 
                NEEDENDIF = NEEDENDIF - 1
 
-           ELSEIF (WANTLABEL(:LENLB) .EQ. 'ENDDO' .AND.
-     &             ANSW(1:5)         .EQ. 'ENDDO') THEN
+           ELSEIF (WANTLABEL(:LENLB) == 'ENDDO' .AND.
+     &             ANSW(1:5)         == 'ENDDO') THEN
 
 C              HUNTING FOR 'ENDDO' AND FOUND IT
                NEEDENDO = NEEDENDO - 1
                !write(6,*) ' needendom1:',needendo
 
 C              IF FOUND CORRESPONDING ENDDO, HALT INPUT
-               IF (NEEDENDO .LE. 0) THEN
+               IF (NEEDENDO <= 0) THEN
                   KEEPGO  = .FALSE.
                   IFLEVEL = IFLEVEL + NEEDENDIF
-                  IF (NEEDENDIF .GT. 0) THEN
+                  IF (NEEDENDIF > 0) THEN
                      WRITE(NOUT,*)' WARNING: JUMP INTO "IF" CLAUSE'
                   ENDIF
               ENDIF
@@ -171,39 +175,39 @@ C              IF FOUND CORRESPONDING ENDDO, HALT INPUT
 C          ----------------------------------------- SEARCHING FOR "LB"
 C          MUST CARE ABOUT IF'S, DO's, 'LB**'s, AND ENDDO's IN HERE
 
-           ELSEIF (ANSW(1:2) .EQ. 'LB' .AND.
-     &         ANSW(:LENANSW) .EQ. WANTLABEL(:LENLB)) THEN
+           ELSEIF (ANSW(1:2) == 'LB' .AND.
+     &             ANSW(:LENANSW) == WANTLABEL(:LENLB)) THEN
 C              HUNTING FOR SPECIFIED 'LB??' AND FOUND IT, HALT INPUT
                KEEPGO  = .FALSE.
 
                IFLEVEL = IFLEVEL + NEEDENDIF
-               IF (NEEDENDIF .GT. 0) THEN
+               IF (NEEDENDIF > 0) THEN
                   WRITE(NOUT,*)' WARNING: JUMP INTO "IF" CLAUSE'
                ENDIF
                !write(6,*) ' found:',ANSW(:LENANSW),':',WANTLABEL(:LENLB),':'
 
-           ELSEIF (WANTLABEL(1:2) .EQ. 'LB' .AND.
-     &             ANSW(1:2)      .EQ. 'IF' .AND.
-     &             IIFTHEN .GT. 5 ) THEN
+           ELSEIF (WANTLABEL(1:2) == 'LB' .AND.
+     &             ANSW(1:2)      == 'IF' .AND.
+     &             IIFTHEN > 5 ) THEN
 C              HUNTING FOR 'LB..' AND FOUND A NEW 'IF...THEN' CLAUSE
                NEEDENDIF = NEEDENDIF + 1
 
-           ELSEIF (WANTLABEL(1:2) .EQ. 'LB' .AND.
-     &             ANSW(1:5)      .EQ. 'ENDIF') THEN
+           ELSEIF (WANTLABEL(1:2) == 'LB' .AND.
+     &             ANSW(1:5)      == 'ENDIF') THEN
 C              HUNTING FOR 'LB..' AND FOUND A 'ENDIF' 
                NEEDENDIF = NEEDENDIF - 1
 
-           ELSEIF (WANTLABEL(1:2) .EQ. 'LB' .AND.
-     &             ANSW(1:2)      .EQ. 'DO' .AND.
-     &             ANSW(3:3)      .NE. 'C'  .AND.
-     &             ANSW(3:4)      .NE. 'LB') THEN
+           ELSEIF (WANTLABEL(1:2) ==  'LB' .AND.
+     &             ANSW(1:2)      ==  'DO' .AND.
+     &             ANSW(3:3)     .NE. 'C'  .AND.
+     &             ANSW(3:4)     .NE. 'LB') THEN
 C              HUNTING FOR 'LB..' AND FOUND A NEW UNLABELED 'DO' CLAUSE
 
                NEEDENDO = NEEDENDO + 1
                !write(6,*) ' needendop1:',needendo
 
-           ELSEIF (WANTLABEL(1:2) .EQ. 'LB' .AND.
-     &             ANSW(1:5)      .EQ. 'ENDDO') THEN
+           ELSEIF (WANTLABEL(1:2) == 'LB' .AND.
+     &             ANSW(1:5)      == 'ENDDO') THEN
 C              HUNTING FOR 'LB..' AND PASSING A "ENDDO" CLAUSE
 
                NEEDENDO = NEEDENDO - 1
@@ -216,26 +220,29 @@ C          -------------------------------- PASS END OF ACTIVE DO-LOOP?
 C          SET NEWLOOP IF WE PASS BY CURRENT DO-LOOP LABEL
            !write(6,*) ' keepgo,nloopt,newloop:',keepgo,nloopt,newloop
 
-	   IF (KEEPGO .AND. NLOOPT .GT. 0) THEN 
-              IF (ANSW(1:2) .EQ. 'LB') THEN
+	   IF (KEEPGO .AND. NLOOPT > 0) THEN 
+              IF (ANSW(1:2) == 'LB') THEN
 C                PASSED BY LINE LABEL, MAY BE END OF A LOOP
                  CALL GETLBNO(ANSW,LBNO,IRTFLG)
+                 IPROCNOW = IDOSTK(5,IDOTOP) 
+
                  DO I = IDOTOP,1,-1
-                    IF (LBNO .EQ. IDOSTK(6,I)) THEN
-C                      FOUND END OF ACTIVE DO-LOOP
+                    ! write(6,*) ' idostk(5,6):',i,idostk(5,i),idostk(6,i)
+                    IF (IPROCNOW > IDOSTK(5,I)) EXIT  ! OUT OF THIS PROC.
+                    IF (LBNO == IDOSTK(6,I)) THEN
+C                      FOUND END OF AN ACTIVE DO-LOOP
                        NEWLOOP = NEWLOOP - 1
-                       !write(6,*) ' LBNO,IDOSTK(6,I):',LBNO,IDOSTK(6,I)
                     ENDIF
                  ENDDO
                  !write(6,990)newloop,idotop,(idostk(6,i),i=1,idotop)
 990              format(' newloop:',i4,' idotop:',i4,' stk:',10i5)
 
-              ELSEIF (ANSW(1:5) .EQ. 'ENDDO') THEN
+              ELSEIF (ANSW(1:5) == 'ENDDO') THEN
 C                PASSED BY ENDDO, END OF A LOOP
 
-                 IF (NEEDENDO .LE. 0) THEN
+                 IF (NEEDENDO <= 0) THEN
 C                   MAY BE A CURRENT ACTIVE DO-LOOP
-                    IF (IDOSTK(6,IDOTOP).LT.0) THEN
+                    IF (IDOSTK(6,IDOTOP) < 0) THEN
 C                       USING LB LESS DO LOOP SYNTAX
                         NEWLOOP = NEWLOOP - 1
                     !write(6,990)newloop,idotop,(idostk(6,i),i=1,idotop)
@@ -251,10 +258,9 @@ C       FOUND DESIRED LABEL OR ENDIF CAN RETURN NOW
 C       SIMULATE ECHO OF OPERATION TO RESULTS FILE
         FCHAR = ANSW
 
-        IF (MYPID .LE. 0) WRITE(NDAT,90) ANSW(1:NCHAR)
+        IF (MYPID <= 0) WRITE(NDAT,90) ANSW(1:NCHAR)
  90     FORMAT(' .OPERATION:',5X,A)
 
-        RETURN
 	END
 
 
@@ -271,7 +277,6 @@ C               LNBLNKN                            MAY  04 ArDean Leith
 C               DOC INSIDE LOOP BUG                JUL  07 ArDean Leith
 C               NINSAVEOF                          NOV  09 ArDean Leith
 C               PASS LOOP END WHILE HUNTING LB     DEC  09 Ardean Leith
-C **********************************************************************
 C **********************************************************************
 C
 C  FINDENDIF(WANTLABEL,IFLEVEL,IRTFLG)
@@ -306,7 +311,7 @@ C       KEEP READING AND DISCARDING LINES FROM INPUT AS NEEDED
         DO WHILE (KEEPGO) 
            ANSW(4:4) = ' '
 
-           IF (COPT .EQ. 'B' .AND. NINSAVEOF .EQ. 0) THEN
+           IF (COPT == 'B' .AND. NINSAVEOF == 0) THEN
 C             INCREMENT PROCEDURE LINE COUNTER
               IBCNT = IBCNT + 1
 
@@ -316,7 +321,7 @@ C             READ FROM CURRENT STORED PROCEDURE LINE= IBCNT
 C             READ FROM FILE OPENED ON NIN (FOR INTERACTIVE LOOP)
               READ(NIN,FMT='(A)',IOSTAT=IOS) ANSW
               NCHAR = LNBLNKN(ANSW)
-              IF (LUNDONOW .GT. 0) THEN
+              IF (LUNDONOW > 0) THEN
 C               MUST COPY INPUT LINE TO CURRENT INTERACTIVE DO-LOOP FILE
                 WRITE(LUNDONOW,*) ANSW(1:NCHAR)
               ENDIF
@@ -335,11 +340,11 @@ C          REMOVE BLANKS FROM INPUT STRING
 
 C          IGNORE ANY COMMENTS AT END OF INPUT STRING
            ISEMICOL = SCAN(ANSW(1:NCHAR),';!')
-           IF (ISEMICOL .GT. 0) THEN
+           IF (ISEMICOL > 0) THEN
               ANSW(ISEMICOL:NCHAR) = CHAR(0)
               NCHAR = ISEMICOL - 1
            ENDIF
-           IF (NCHAR .LE. 0) CYCLE
+           IF (NCHAR <= 0) CYCLE
 
 C          CONVERT INPUT STRING TO ALL UPPER CASE
            CALL SSUPCAS(ANSW(1:NCHAR))
@@ -353,35 +358,35 @@ C          FIND LAST NON-BLANK IN WANTLABEL
 C          FIND LAST NON-BLANK IN ANSW            
            LENANSW = LNBLNKN(ANSW)
            LENT    = INDEX(ANSW,CHAR(0)) - 1
-           IF (LENT .GT. 0) LENANSW = MIN(LENANSW,LENT)
+           IF (LENT > 0) LENANSW = MIN(LENANSW,LENT)
 
 C          -------------------------------------- SEARCHING FOR "ELSE"
 
-           IF (WANTLABEL .EQ. 'ELSE' .AND.
-     &             ANSW(1:6) .EQ. 'ELSEIF') THEN
+           IF (WANTLABEL == 'ELSE' .AND.
+     &             ANSW(1:6) == 'ELSEIF') THEN
 C              HUNTING FOR 'ELSE' AND FOUND 'ELSEIF'
 
 C              IF NOT A NESTED ELSE, RETURN TO CALLER
-               IF (NEEDENDIF .LE. 0)  THEN
+               IF (NEEDENDIF <= 0)  THEN
                    KEEPGO = .FALSE.
 
 C                  DECREMENT PROCEDURE LINE COUNTER TO RE-READ THIS LINE
-                   IF (COPT .EQ. 'B' .AND. NINSAVEOF .EQ. 0)
+                   IF (COPT == 'B' .AND. NINSAVEOF == 0)
      &                 IBCNT = IBCNT-1
                ENDIF
 
-           ELSEIF (WANTLABEL .EQ. 'ELSE' .AND.
-     &             ANSW(1:4) .EQ. 'ELSE') THEN
+           ELSEIF (WANTLABEL == 'ELSE' .AND.
+     &             ANSW(1:4) == 'ELSE') THEN
 C              HUNTING FOR 'ELSE' AND FOUND IT
 C              IF NOT A NESTED ELSE, RETURN TO CALLER
-               IF (NEEDENDIF .LE. 0)  KEEPGO = .FALSE.
+               IF (NEEDENDIF <= 0)  KEEPGO = .FALSE.
 
-           ELSEIF (WANTLABEL .EQ. 'ELSE' .AND.
-     &             ANSW(1:5) .EQ. 'ENDIF') THEN
+           ELSEIF (WANTLABEL == 'ELSE' .AND.
+     &             ANSW(1:5) == 'ENDIF') THEN
 C              HUNTING FOR 'ELSE' AND FOUND ENDIF
 
 C              IF NOT A NESTED ENDIF, RETURN TO CALLER
-               IF (NEEDENDIF .LE. 0) THEN
+               IF (NEEDENDIF <= 0) THEN
                   IFLEVEL = IFLEVEL - 1
                   KEEPGO  = .FALSE.
                ENDIF
@@ -389,9 +394,9 @@ C              IF NOT A NESTED ENDIF, RETURN TO CALLER
 C              IF NESTED ENDIF KEEP READING INPUT
                NEEDENDIF = NEEDENDIF - 1
 
-           ELSEIF (WANTLABEL .EQ. 'ELSE' .AND.
-     &             ANSW(1:2) .EQ. 'IF'   .AND.
-     &             IIFTHEN .GT. 5 ) THEN
+           ELSEIF (WANTLABEL == 'ELSE' .AND.
+     &             ANSW(1:2) == 'IF'   .AND.
+     &             IIFTHEN > 5 ) THEN
 C              HUNTING FOR 'ELSE' AND FOUND IF...THEN
 C              THIS IS A NESTED IF,  KEEP READING INPUT
                NEEDENDIF = NEEDENDIF + 1
@@ -399,18 +404,18 @@ C              THIS IS A NESTED IF,  KEEP READING INPUT
 C          -------------------------------------- SEARCHING FOR "ENDIF"
 C          DO NOT CARE ABOUT NEW LOOPS, OR LABELS IN HERE
 
-           ELSEIF (WANTLABEL .EQ. 'ENDIF' .AND.
-     &             ANSW(1:2) .EQ. 'IF'    .AND.
-     &            IIFTHEN .GT. 5 ) THEN
+           ELSEIF (WANTLABEL == 'ENDIF' .AND.
+     &             ANSW(1:2) == 'IF'    .AND.
+     &            IIFTHEN > 5 ) THEN
 C              HUNTING FOR 'ENDIF' AND FOUND A NESTED IF...THEN
                NEEDENDIF = NEEDENDIF + 1
 
-           ELSEIF (WANTLABEL .EQ. 'ENDIF' .AND.
-     &             ANSW(1:5) .EQ. 'ENDIF') THEN
+           ELSEIF (WANTLABEL == 'ENDIF' .AND.
+     &             ANSW(1:5) == 'ENDIF') THEN
 C              HUNTING FOR 'ENDIF' AND FOUND ENDIF
 
 C              IF NOT A NESTED ENDIF, RETURN TO CALLER
-               IF (NEEDENDIF .LE. 0) THEN
+               IF (NEEDENDIF <= 0) THEN
                   KEEPGO  = .FALSE.
                   IFLEVEL = IFLEVEL - 1
                ENDIF
@@ -427,8 +432,7 @@ C       FOUND DESIRED 'ELSE' OR 'ENDIF' CAN RETURN NOW
 C       SIMULATE ECHO OF OPERATION TO RESULTS FILE
         FCHAR = ANSW
 
-        IF (MYPID .LE. 0) WRITE(NDAT,90) ANSW(1:NCHAR)
+        IF (MYPID <= 0) WRITE(NDAT,90) ANSW(1:NCHAR)
  90     FORMAT(' .OPERATION:',5X,A)
 
-        RETURN
 	END
