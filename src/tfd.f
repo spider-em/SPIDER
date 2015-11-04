4376eb3d8832c0f0b3633908db5cfa164f2444b4
ArDean Leith <agleith@gmail.com>
2015-11-04 10:10:53 -0500

    prompts

--- src/tfd.f
+++ src/tfd.f
@@ -1,12 +1,18 @@
 C++*********************************************************************
 C
-C $$ TFD.FOR
+C TFD.FOR
+C        CHANGED TO INCLUDE INTELLIGIBLE GAUSSIAN PARAMETER JF 9/5/94
+C        CHANGED LAST LINE TO CORRECT NORMALIZATION 4/2/03
+C        WGH PASSED IN BY CALLING ROUTINE AS ATAN(WGH/(1.0-WGH)),
+C        CS1 PASSED IN BY CALLING ROUTINE AS CS*1.E7,
+C           (16.*ALOG(2.)) CHANGED TO PARAMETER KPP,
+C        F = -QUADPI**2 CHANGED TO A PARAMETER
 C
 C **********************************************************************
 C=*                                                                    *
 C=* This file is part of:   SPIDER - Modular Image Processing System.  *
 C=* SPIDER System Authors:  Joachim Frank & ArDean Leith               *
-C=* Copyright 1985-2010  Health Research Inc.,                         *
+C=* Copyright 1985-2015  Health Research Inc.,                         *
 C=* Riverview Center, 150 Broadway, Suite 560, Menands, NY 12204.      *
 C=* Email: spider@wadsworth.org                                        *
 C=*                                                                    *
@@ -24,46 +30,43 @@ C=* along with this program. If not, see <http://www.gnu.org/licenses> *
 C=*                                                                    *
 C **********************************************************************
 C
-C
-C CHANGED 9/5/94 TO INCLUDE INTELLIGIBLE
-C GAUSSIAN PARAMETER JF
-C
-C CHANGED LAST LINE TO CORRECT NORMALIZATION 4/2/03
-C    WGH passed in by calling routine as ATAN(WGH/(1.0-WGH)),
-C    CS1 passed in by calling routine as CS*1.E7,
-C    (16.*ALOG(2.)) changed to parameter KPP,
-C    F = -QUADPI**2 changed to a parameter
-C
+C TFD(B,CS1,DZ,LAMBDA,Q,DS,IE,AK,WGH,ENV)
 C
 C23456789012345678901234567890123456789012345678901234567890123456789012
 C--*********************************************************************
 
          SUBROUTINE TFD(B,CS1,DZ,LAMBDA,Q,DS,IE,AK,WGH,ENV)
 
-         REAL LAMBDA,KAPPA
+         REAL       LAMBDA,KAPPA
          PARAMETER (QUADPI = 3.141592653589793238462643383279502884197)
          PARAMETER (KPP = 11.09035491943359380000)
          PARAMETER (F = -9.8696050643920898400000)
 
-C         CS1 = CS*1.E7
-         F1=1./SQRT(CS1*LAMBDA)
-         F2=SQRT(SQRT(CS1*LAMBDA**3))
-         Q1 = (Q*F2)**2
-         ENV1=ENV/F2**2
-         AKK=AK*F2
-C         F=-QUADPI**2
-         DS1=DS*DS*F1
-         KAPPA=DS1*F/KPP
-         DZ1=DZ*F1
-         P=AKK**3-DZ1*AKK
-         CH=EXP(AKK**4*KAPPA)
-C         B=(EXP(F*Q1*P**2)*CH)*2*EXP(-ENV1*AKK**2)
-C Factor "2" was removed to have the CTF=W at zero.
-         B=(EXP(F*Q1*P**2)*CH)*EXP(-ENV1*AKK**2)
-         IF(IE.EQ.1) RETURN
-         QQT=2.*QUADPI*(.25*AKK**4-.5*DZ1*AKK**2)
-         B=B*SIN(QQT-WGH)
-C         B=B*SIN(QQT-ATAN(WGH/(1.0-WGH)))       ; old
-C         B=B*((1.0-WGH)*SIN(QQT)-WGH*COS(QQT))  ; older
+C        CS1   = CS * 1.E7
+         F1    = 1. / SQRT(CS1 * LAMBDA)
+         F2    = SQRT(SQRT(CS1 * LAMBDA**3))
+         Q1    = (Q * F2)**2
+         ENV1  = ENV / F2**2
+         AKK   = AK * F2
+C        F     = -QUADPI**2
+         DS1   = DS  * DS * F1
+         KAPPA = DS1 * F / KPP
+         DZ1   = DZ  * F1
+         P     = AKK**3 - DZ1 * AKK
+         CH    = EXP(AKK**4 * KAPPA)
+
+C        B     = (EXP(F*Q1*P**2)*CH)*2*EXP(-ENV1*AKK**2)
+
+C        FACTOR "2" WAS REMOVED TO HAVE THE CTF = W AT ZERO.
+
+         B     = (EXP(F * Q1 * P**2) * CH) * EXP(-ENV1 * AKK**2)
+         IF (IE == 1) RETURN
+
+         QQT   = 2. * QUADPI*(.25 * AKK**4 - .5 * DZ1 * AKK**2)
+         B      =B * SIN(QQT - WGH)
+
+C        B     = B * SIN(QQT - ATAN(WGH / (1.0 - WGH)))       ; OLD
+C        B     = B * ((1.0 - WGH) * SIN(QQT) - WGH*COS(QQT))  ; OLDER
+
          END
 
